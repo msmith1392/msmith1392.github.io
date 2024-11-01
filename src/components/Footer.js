80d958f656d8087f9798768973100b1a09afa189
Matthew Smith <smithm@4arrowsinc.com>
2024-10-31 20:31:38 -0400

    Add scroll-to-top behavior in Footer nav links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
+const scrollToTop = () => {
+  window.scrollTo(0, 0);
+};  
+
 const Footer = () => {
   return (
     <footer>
       <nav>
         <ul>
           <li>
-            <Link to="/">About</Link>
+            <Link to="/" onClick={scrollToTop}>About</Link>
           </li>
           <li>
-            <Link to="/work-experience">Work Experience</Link>
+            <Link to="/work-experience" onClick={scrollToTop}>Work Experience</Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact" onClick={scrollToTop}>Contact</Link>
           </li>
         </ul>
       </nav>
