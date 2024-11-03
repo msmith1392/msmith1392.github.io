5a0540781b3c8366f384dbf5c75953db2c41abf5
Matthew Smith <smithm@4arrowsinc.com>
2024-11-03 01:09:22 -0400

    NavBar links now scroll to top of page on click

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const NavBar = () => (
   <nav>
     <ul>
-      <li><Link to="/">About</Link></li>
-      <li><Link to="/work-experience">Work Experience</Link></li>
-      <li><Link to="/contact">Contact</Link></li>
+      <li><Link to="/" onClick={() => window.scrollTo(0, 0)}>About</Link></li>
+      <li><Link to="/work-experience" onClick={() => window.scrollTo(0, 0)}>Work Experience</Link></li>
+      <li><Link to="/contact" onClick={() => window.scrollTo(0, 0)}>Contact</Link></li>
     </ul>
   </nav>
 );
