41e0fb9041fd373505d573e4c2697c9aa140f258
Matthew Smith <smithm@4arrowsinc.com>
2025-06-29 00:43:34 -0400

    Updated resume and contact email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,7 +13,7 @@ const Contact = () => {
       <FadeInSection>
         <h2>Contact Information</h2>
         <ul className="list-unstyled">
-          <li>Email: <a href="mailto:baseball.bats129@silomails.com">baseball.bats129@silomails.com</a></li>
+          <li>Email: <a href="mailto:msmith1392@gmail.com">msmith1392@gmail.com</a></li>
           <li>GitHub: <a href="https://github.com/msmith1392" target="_blank" rel="noopener noreferrer">View My GitHub Profile</a></li>
           <li>Resume: <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">Download PDF</a></li>
         </ul>
